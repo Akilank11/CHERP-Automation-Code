74ee34d58c444784c51ba6700c123009dc2d4570
PANINI019 <PANINI019@PANINI18.PANINI.COM>
2025-01-22 16:30:14 +0530

    Initial Commit

--- cherpsystem/src/test/java/Cherpsystem/cherpsystem/Facilitycontract.java
+++ cherpsystem/src/test/java/Cherpsystem/cherpsystem/Facilitycontract.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Facilitycontract {
+	
 	WebDriver driver = new ChromeDriver();
 //FirefoxOptions firefoxoptions = new FirefoxOptions();
 //ChromeOptions chromeoptions = new ChromeOptions();
