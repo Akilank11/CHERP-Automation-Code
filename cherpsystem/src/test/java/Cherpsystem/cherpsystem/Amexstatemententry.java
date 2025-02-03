bdcb390973d8e15ff44287f30fdf18f1e1196214
PANINI019 <PANINI019@PANINI18.PANINI.COM>
2025-02-03 18:18:50 +0530

    amex statement entry

---
+++ cherpsystem/src/test/java/Cherpsystem/cherpsystem/Amexstatemententry.java
@@ -0,0 +1,68 @@
+package Cherpsystem.cherpsystem;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Amexstatemententry {WebDriver driver = new ChromeDriver();
+//FirefoxOptions firefoxoptions = new FirefoxOptions();
+//ChromeOptions chromeoptions = new ChromeOptions();
+//EdgeOptions edgeoptions = new EdgeOptions();
+
+//@Parameters("browser")
+//@BeforeTest
+
+//public void initialize(String browser) 
+
+//{
+	 //if (browser.equalsIgnoreCase("chrome")) {
+        
+          //driver = new ChromeDriver();
+      //} else if (browser.equalsIgnoreCase("firefox")) {
+          
+          //driver = new FirefoxDriver();
+      //} else if (browser.equalsIgnoreCase("edge")) {
+         
+         // driver = new EdgeDriver();
+      //}
+      //driver.manage().window().maximize();
+  //}
+
+  @Test (enabled=true)
+  public void loginTest() throws InterruptedException {
+      driver.get("http://localhost:8080/CHERPSystem/login");
+      driver.findElement(By.id("userName")).sendKeys("superadmin@gmail.com");
+      driver.findElement(By.id("password")).sendKeys("Dev@9090");
+      //WebElement rememberme =driver.findElement(By.id("rememberMe1"));
+  	// rememberme.click();
+  	driver.findElement(By.id("btn-login")).click();
+  	driver.manage().window().maximize();
+  	
+  //Click Amex statement entry
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+ 	WebElement Amex = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='#expenseTran']")));
+ 	Amex.click(); 
+ 	Thread.sleep(2000);
+ 	WebElement amexstatemententry = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='AMEX Statement Entry']"))); 
+ 	amexstatemententry.click();
+Thread.sleep(2000);
+
+//Search
+WebElement employee = driver.findElement(By.name("empId"));
+Select employeeselect = new Select(employee);
+employeeselect.selectByVisibleText("Lynn M Junca");
+WebElement search = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("newSearch")));
+search.click(); 
+Thread.sleep(2000);
+WebElement clear = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("clearSearch")));
+clear.click();
+Thread.sleep(2000);
+
+  }}
