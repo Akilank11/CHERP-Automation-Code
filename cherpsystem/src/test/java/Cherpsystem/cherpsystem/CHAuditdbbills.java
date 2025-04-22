20edeb11b3e06528d66693dc264def60027e4bcd
PANINI019 <PANINI019@PANINI18.PANINI.COM>
2025-04-22 16:21:41 +0530

    CH Audit DB Bills

---
+++ cherpsystem/src/test/java/Cherpsystem/cherpsystem/CHAuditdbbills.java
@@ -0,0 +1,100 @@
+package Cherpsystem.cherpsystem;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class CHAuditdbbills {
+	WebDriver driver = new ChromeDriver();
+	//FirefoxOptions firefoxoptions = new FirefoxOptions();
+	//ChromeOptions chromeoptions = new ChromeOptions();
+	//EdgeOptions edgeoptions = new EdgeOptions();
+
+	//@Parameters("browser")
+	//@BeforeTest
+
+	//public void initialize(String browser) 
+
+	//{
+		 //if (browser.equalsIgnoreCase("chrome")) {
+	          
+	            //driver = new ChromeDriver();
+	        //} else if (browser.equalsIgnoreCase("firefox")) {
+	            
+	            //driver = new FirefoxDriver();
+	        //} else if (browser.equalsIgnoreCase("edge")) {
+	           
+	           // driver = new EdgeDriver();
+	        //}
+	        //driver.manage().window().maximize();
+	    //}
+
+	    @Test 
+	    public void loginTest() throws InterruptedException {
+	        driver.get("http://localhost:8080/CHERPSystem/login");
+	        driver.findElement(By.id("userName")).sendKeys("superadmin@gmail.com");
+	        driver.findElement(By.id("password")).sendKeys("Dev@8080");
+	        WebElement rememberme =driver.findElement(By.id("rememberMe1"));
+	    	rememberme.click();
+	    	driver.findElement(By.id("btn-login")).click();
+	    	driver.manage().window().maximize();
+	    	
+	    	//Click Invoices  
+	    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		 	WebElement Reports = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Reports']")));
+		 	Reports.click(); 
+		 	Thread.sleep(3000);
+		 	WebElement Spclaims = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='SP Claims']"))); 
+		 	Spclaims.click();
+		 	WebElement CHAuditDBBills= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='CH Audit Db Bills']"))); 
+		 	CHAuditDBBills.click();
+	Thread.sleep(2000);
+	
+
+	
+	driver.findElement(By.id("dateFrom")).click();
+    WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
+    WebElement InvoiceFromDate = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@type='button'])[4]")));
+    InvoiceFromDate.click();
+    InvoiceFromDate.click();
+    WebElement InvoiceFromDateselect = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("td[data-date='17'][data-month='1'][data-year='2025']")));
+    InvoiceFromDateselect.click();
+    Thread.sleep(2000);
+    
+    driver.findElement(By.id("dosStartDate")).click();
+    WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(10));
+    WebElement Dosstartdate = wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@type='button'])[10]")));
+    Dosstartdate.click();
+    Dosstartdate.click();
+    Dosstartdate.click();
+    WebElement Dosstartdateselect = wait2.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("td[data-date='17'][data-month='0'][data-year='2025']")));
+    Dosstartdateselect.click();
+    Thread.sleep(3000);
+    
+    driver.findElement(By.id("dosEndDate")).click();
+   WebElement Dosenddateselect = wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@type='button'])[16]")));
+   Dosenddateselect.click();
+    WebElement Dosenddateselectselect = wait2.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("td[data-date='17'][data-month='2'][data-year='2025']")));
+    Dosenddateselectselect.click();
+    Thread.sleep(3000);
+    
+    WebElement Facility = driver.findElement(By.id("facilityCode"));
+	Select Facilityselect = new Select(Facility);
+	Facilityselect.selectByVisibleText("Acadia Parish Jail");
+	
+
+	
+	
+driver.findElement(By.id("pushChAuditDbBillsBtn")).click();
+	 Thread.sleep(1000);
+	
+	driver.findElement(By.xpath("//button[normalize-space()='Clear']")).click();
+	
+	    }}
---
+++ cherpsystem/src/test/java/Cherpsystem/cherpsystem/ContractsRenewalAlert.java
@@ -0,0 +1,63 @@
+package Cherpsystem.cherpsystem;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class ContractsRenewalAlert {
+	WebDriver driver = new ChromeDriver();
+	//FirefoxOptions firefoxoptions = new FirefoxOptions();
+	//ChromeOptions chromeoptions = new ChromeOptions();
+	//EdgeOptions edgeoptions = new EdgeOptions();
+
+	//@Parameters("browser")
+	//@BeforeTest
+
+	//public void initialize(String browser) 
+
+	//{
+		 //if (browser.equalsIgnoreCase("chrome")) {
+	        
+	          //driver = new ChromeDriver();
+	      //} else if (browser.equalsIgnoreCase("firefox")) {
+	          
+	          //driver = new FirefoxDriver();
+	      //} else if (browser.equalsIgnoreCase("edge")) {
+	         
+	         // driver = new EdgeDriver();
+	      //}
+	      //driver.manage().window().maximize();
+	  //}
+
+	  @Test
+	  public void loginTest() throws InterruptedException {
+	      driver.get("http://localhost:8090/CHERPSystem/login");
+	      driver.findElement(By.id("userName")).sendKeys("superadmin@gmail.com");
+	      driver.findElement(By.id("password")).sendKeys("Dev@9090");
+	      //WebElement rememberme =driver.findElement(By.id("rememberMe1"));
+	  	// rememberme.click();
+	  	driver.findElement(By.id("btn-login")).click();
+	  	driver.manage().window().maximize();
+	  	
+	  //Click Contracts Renewal Alert
+
+
+      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement Reports= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Reports']")));
+		Reports.click(); 
+	 	WebElement Facilitycontract= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[contains(text(),'Facility Contracts')])[2]"))); 
+	 	Facilitycontract.click();
+	Thread.sleep(1000);
+	WebElement ContractsRenewalAlert= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='Contracts Renewal Alert Report']"))); 
+	ContractsRenewalAlert.click();
+	Thread.sleep(2000);
+	
+	driver.findElement(By.id("newSearch")).click();
+  }
+}
